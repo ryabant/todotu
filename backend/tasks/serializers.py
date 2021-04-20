50872017c7a7c2a4c31abca7fc9eb00bd4738b19
Anton <ryabant@gmail.com>
2021-04-20 21:19:38 +0300

    update frontend

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -2,13 +2,17 @@ from rest_framework import serializers
 from .models import Board, Task
 
 
-class BoardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Board
-        fields = ["id", "name", "owner"]
-
-
 class TaskSerializer(serializers.ModelSerializer):
+    board = serializers.PrimaryKeyRelatedField(queryset=Board.objects.all())
+
     class Meta:
         model = Task
         fields = ["id", "body", "board", "created", "completed"]
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True)
+
+    class Meta:
+        model = Board
+        fields = ["id", "name", "owner", "tasks"]
