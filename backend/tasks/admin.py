70cd18763982291d70fcb35dc7cdf064f5b68dd7
Anton <ryabant@gmail.com>
2021-05-07 00:39:35 +0300

    update backend

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Board, Task
+from .models import Board, Task, Tag
 
 admin.site.register(Board)
 admin.site.register(Task)
+admin.site.register(Tag)
--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -16,9 +16,26 @@ class Board(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    color = models.CharField(max_length=7)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name="tags")
+
+
+class Priority(models.TextChoices):
+    HIGH = "H", "High"
+    MEDIUM = "M", "Medium"
+    LOW = "L", "Low"
+
+
 class Task(models.Model):
-    body = models.TextField()
+    title = models.CharField(max_length=255)
+    body = models.TextField(blank=True)
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="tasks")
+    priority = models.CharField(
+        max_length=1, choices=Priority.choices, default=Priority.MEDIUM
+    )
+    tags = models.ManyToManyField(Tag, related_name="tasks")
     created = models.DateTimeField(default=timezone.now)
     completed = models.BooleanField(default=False)
 
@@ -26,4 +43,4 @@ class Task(models.Model):
         ordering = ["-created"]
 
     def __str__(self):
-        return self.body
+        return self.title
