1d06b6f6b966fc64795165627bfec1f2e0501d65
ryabant <ryabant@gmail.com>
2021-05-10 18:45:13 +0300

    add services.yml

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -9,14 +9,15 @@ SECRET_KEY = "django-insecure-vk(e%d&e-!4p2(+oh-e7i+@qxf_g9-hwmc5exw_8gliu^sz!xz
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todotu",
+        "USER": "todotu",
+        "PASSWORD": "todotu",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
-
-
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
 ]
-
